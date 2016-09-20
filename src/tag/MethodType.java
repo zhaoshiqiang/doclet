eb914b3209ecd6033f08d9ac8ae95443123d2ea1
风 <523618033@qq.com>
2016-09-20 10:32:15 +0800

    Revert "Revert "在MethodtYPE""
    This reverts commit c084fe80bbf5f75c4ccc54ce7aba6db62bfe1d0e.

--- src/tag/MethodType.java
+++ src/tag/MethodType.java
@@ -17,6 +17,7 @@ public class MethodType implements Serializable{
     * */
     private static final long serialVersionUID = 7991552226614088458L;
     private List<Tag> tagList;
+    private List<Tag> classList;
     private static Map<String,String> template;
     private Map<String,String> outpath;
 
@@ -26,6 +27,14 @@ public class MethodType implements Serializable{
 
     public void setTagList(List<Tag> tagList) {
         this.tagList = tagList;
+    }
+
+    public List<Tag> getClassList() {
+        return classList;
+    }
+
+    public void setClassList(List<Tag> classList) {
+        this.classList = classList;
     }
 
     public static Map<String, String> getTemplate() {
@@ -46,10 +55,15 @@ public class MethodType implements Serializable{
 
 
     public Map<String,List> transformTagvalue(){
-        Map<String,List> map = new HashMap<>();
+        Map<String,List> map = new HashMap<String,List>();
         for (Tag tag : tagList){
             map.put(tag.getName(),tag.transformTagvalue());
 //            System.out.println(tag.getName()+"---->"+tag.transformTagvalue());
+        }
+
+        for (Tag tag : classList){
+            map.put("class_" + tag.getName(), tag.transformTagvalue());
+//            System.out.println(tag.getName() + "---->" + tag.transformTagvalue());
         }
         return map;
     }
